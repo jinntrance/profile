0a234e0044f8349f97cae0b709277b1fc3bdd5fe
Joseph J.C. Tang <jinntrance@gmail.com>
2015-03-26 00:15:26 +0800

    add new analytics js

---
+++ assets/js/analytics.js
@@ -0,0 +1,19 @@
+//i<!-- Asynchronous Google Analytics snippet -->
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-39438576-1', 'auto');  
+ga('require', 'linkid', 'linkid.js');
+ga('send', 'pageview');
+
+//<!-- Asynchronous Baidu Analytics snippet -->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?11224c4b3c0923fa6de6e431325fe387";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
