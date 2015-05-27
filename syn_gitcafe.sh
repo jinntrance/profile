b090115f0f4f86e673d6fb775a97a2373817746f
Joseph J.C. Tang <jinntrance@gmail.com>
2015-05-27 23:52:19 +0800

    update sync script

--- syn_gitcafe.sh
+++ syn_gitcafe.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 #git remote add  gc  git@gitcafe.com:jinntrance/jinntrance.git
+#git remote add profile git@github.com:jinntrance/profile.git
 git pull 
 git push
 git checkout gitcafe-pages
 git merge master
 git push gc 
+git checkout gh-pages 
+git merge master
+git push profile
 git checkout master
