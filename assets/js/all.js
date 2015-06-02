351274eba256a2c1dffdb283ed2d66957c797d3b
Joseph J.C. Tang <jinntrance@gmail.com>
2015-06-02 11:29:54 +0800

    remove baidu push for profile page

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,6 +5,3 @@
 {% include_relative bootstrap.min.js %}
 {% include_relative grayscale.min.js %}
 
-url = '{{site.baidu_push}}'
-$.post(url, $('a').map(function(i,a){return a.href}).toArray().join('\n'))
-$.post(url, window.location.href)
