f02f8e82c6e2edc38b88b79d85d10f1d3494e963
Joseph J.C. Tang <jinntrance@gmail.com>
2015-11-23 10:19:52 +0800

    add CNZZ analytics

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,3 +1,15 @@
+/* <!-- Asynchronous Baidu Analytics snippet --> */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?11224c4b3c0923fa6de6e431325fe387";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+/* <!-- Aysnchronous CNZZ Analytics  -->  */
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1256833726'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1256833726' type='text/javascript'%3E%3C/script%3E"));</script>
+
 /* <!-- Asynchronous Google Analytics snippet --> */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -8,11 +20,3 @@ ga('create', 'UA-39438576-1', 'auto');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
 
-/* <!-- Asynchronous Baidu Analytics snippet --> */
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?11224c4b3c0923fa6de6e431325fe387";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
