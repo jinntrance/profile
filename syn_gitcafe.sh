425ae73414ad4274a014d6f1d74ff81f751953be
Joseph J.C. Tang <jinntrance@gmail.com>
2016-12-07 01:07:28 +0800

    merge different branch

--- syn_gitcafe.sh
+++ syn_gitcafe.sh
@@ -3,10 +3,10 @@
 #git remote add profile git@github.com:jinntrance/profile.git
 git pull 
 git push
-git checkout coding-pages
-git merge master
-git push coding 
 git checkout gh-pages 
 git merge master
 git push profile
+git checkout coding-pages
+git merge gh-pages
+git push coding 
 git checkout master
