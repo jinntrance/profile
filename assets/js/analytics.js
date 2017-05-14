8b7f66155233fb8e02e49dcc8886eaee0855bacb
Joseph J.C. Tang <jinntrance@gmail.com>
2017-05-14 20:23:36 +0800

    update profile

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -3,8 +3,8 @@ var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
   hm.async=1;
-  hm.src = "//hm.baidu.com/hm.js?11224c4b3c0923fa6de6e431325fe387";
-  var s = document.getElementsByTagName("script")[0]; 
+  hm.src = "https://hm.baidu.com/hm.js?11224c4b3c0923fa6de6e431325fe387";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
 
@@ -13,8 +13,8 @@ var _hmt = _hmt || [];
   /* <!-- Aysnchronous CNZZ Analytics  -->  */
   var za = document.createElement("script");
   za.async=1;
-  za.src = "//s95.cnzz.com/z_stat.php?id=1256833726&web_id=1256833726";
-  var s = document.getElementsByTagName("script")[0]; 
+  za.src = "https://s95.cnzz.com/z_stat.php?id=1256833726&web_id=1256833726";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(za, s);
 })(document);
 
@@ -22,9 +22,8 @@ var _hmt = _hmt || [];
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-39438576-1', 'auto');  
+ga('create', 'UA-39438576-1', 'auto');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
-
